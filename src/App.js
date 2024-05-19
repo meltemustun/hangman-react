a879307defbfe7daa8b9841f86ef71fc92ba98f1
meltemustun <meltemustundev@gmail.com>
2024-05-19 22:22:36 +0200

    hangman with react

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
-import React, { useState, useEffect } from 'react';
-import Header from './components/Header';
-import Figure from './components/Figure';
-import WrongLetters from './components/WrongLetters';
-import Word from './components/Word';
-import Popup from './components/Popup';
-import Notification from './components/Notification';
-import { showNotification as show, checkWin } from './helpers/helpers';
+import React, { useState, useEffect } from "react";
+import Header from "./components/Header";
+import Figure from "./components/Figure";
+import WrongLetters from "./components/WrongLetters";
+import Word from "./components/Word";
+import Popup from "./components/Popup";
+import Notification from "./components/Notification";
+import { showNotification as show, checkWin } from "./helpers/helpers";
 
-import './App.css';
+import "./App.css";
 
-const words = ['application', 'programming', 'interface', 'wizard'];
+const words = ["application", "programming", "interface", "wizard"];
 let selectedWord = words[Math.floor(Math.random() * words.length)];
 
 function App() {
@@ -19,34 +19,33 @@ function App() {
   const [showNotification, setShowNotification] = useState(false);
 
   useEffect(() => {
-    const handleKeydown = event => {
+    const handleKeydown = (event) => {
       const { key, keyCode } = event;
       if (playable && keyCode >= 65 && keyCode <= 90) {
         const letter = key.toLowerCase();
+
         if (selectedWord.includes(letter)) {
           if (!correctLetters.includes(letter)) {
-            setCorrectLetters(currentLetters => [...currentLetters, letter]);
+            setCorrectLetters([...correctLetters, letter]);
           } else {
             show(setShowNotification);
           }
+        } else if (!wrongLetters.includes(letter)) {
+          setWrongLetters([...wrongLetters, letter]);
         } else {
-          if (!wrongLetters.includes(letter)) {
-            setWrongLetters(currentLetters => [...currentLetters, letter]);
-          } else {
-            show(setShowNotification);
-          }
+          show(setShowNotification);
         }
       }
-    }
-    window.addEventListener('keydown', handleKeydown);
+    };
 
-    return () => window.removeEventListener('keydown', handleKeydown);
+    window.addEventListener("keydown", handleKeydown);
+
+    return () => window.removeEventListener("keydown", handleKeydown);
   }, [correctLetters, wrongLetters, playable]);
 
   function playAgain() {
     setPlayable(true);
 
-    // Empty Arrays
     setCorrectLetters([]);
     setWrongLetters([]);
 
@@ -60,9 +59,15 @@ function App() {
       <div className="game-container">
         <Figure wrongLetters={wrongLetters} />
         <WrongLetters wrongLetters={wrongLetters} />
-        <Word selectedWord={selectedWord} correctLetters={correctLetters} />
+        <Word correctLetters={correctLetters} selectedWord={selectedWord} />
       </div>
-      <Popup correctLetters={correctLetters} wrongLetters={wrongLetters} selectedWord={selectedWord} setPlayable={setPlayable} playAgain={playAgain} />
+      <Popup
+        correctLetters={correctLetters}
+        selectedWord={selectedWord}
+        wrongLetters={wrongLetters}
+        setPlayable={setPlayable}
+        playAgain={playAgain}
+      />
       <Notification showNotification={showNotification} />
     </>
   );
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,12 @@
-import React from 'react'
+import React from "react";
 
-// rafce
 const Header = () => {
   return (
     <>
       <h1>Hangman</h1>
       <p>Find the hidden word - Enter a letter</p>
     </>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
